f67e072f88aea79a487565b94205c5695b62058e
yuririnnnu <yrk96424@gmail.com>
2022-05-15 18:26:02 -0400

    corrected formatting

--- lotto_ai/fetch_historical_nums.py
+++ lotto_ai/fetch_historical_nums.py
@@ -5,14 +5,17 @@ import json
 from sys import argv
 from datetime import datetime
 
+
 def get_date(date_string):
     date_object = datetime.strptime(date_string[0:10], '%Y-%m-%d')
     return date_object.strftime('%d-%b-%y')
+
 
 def get_numbers(numbers_string):
     numbers = numbers_string.split(' ')
     number_array = [int(num) for num in numbers]
     return ', '.join([str(num) for num in number_array])
+
 
 def fetch_numbers(lotto_type, latest_only):
     if lotto_type == 'megamillion':
@@ -22,7 +25,8 @@ def fetch_numbers(lotto_type, latest_only):
 
         if latest_only.lower() == 'true':
             date = get_date(json_object[0]['draw_date'])
-            numbers = get_numbers(json_object[0]['winning_numbers']) + ', ' + json_object[0]['mega_ball']
+            numbers = get_numbers(
+                json_object[0]['winning_numbers']) + ', ' + json_object[0]['mega_ball']
             multiplier = json_object[0]['multiplier']
             multiplier = str(int(multiplier)) if multiplier else '1'
             string = f"'{date}', {numbers}, {multiplier}"
@@ -31,12 +35,13 @@ def fetch_numbers(lotto_type, latest_only):
         else:
             for row in json_object:
                 date = get_date(row['draw_date'])
-                numbers = get_numbers(row['winning_numbers'])  + ', ' + row['mega_ball']
-                multiplier = str(int(row['multiplier']))                
+                numbers = get_numbers(
+                    row['winning_numbers']) + ', ' + row['mega_ball']
+                multiplier = str(int(row['multiplier']))
                 multiplier = str(int(multiplier)) if multiplier else '1'
                 string = f"'{date}', {numbers}, {multiplier}"
                 insert_winning_pairs(lotto_type, string)
-            
+
     elif lotto_type == 'powerball':
         url = 'https://data.ny.gov/resource/d6yy-54nr.json'
         r = requests.get(url)
@@ -60,7 +65,8 @@ def fetch_numbers(lotto_type, latest_only):
                     multiplier = '1'
                 string = f"'{date}', {numbers}, {multiplier}"
                 insert_winning_pairs(lotto_type, string)
-                
+
     connection.commit()
+
 
 fetch_numbers(argv[1], argv[2])
