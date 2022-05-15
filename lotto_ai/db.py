93caccd4322a072d8a9ed6392667ac02754e5fb1
yuririnnnu <yrk96424@gmail.com>
2022-05-14 21:59:25 -0400

    added function for inserting predected pairs

--- lotto_ai/db.py
+++ lotto_ai/db.py
@@ -9,7 +9,7 @@ cx_Oracle.init_oracle_client(os.environ.get('HOME') + '/Downloads/instantclient_
 connection = cx_Oracle.connect(user=os.getenv('oracle_user'), password=os.getenv('oracle_password'), dsn=os.getenv('oracle_dsn'))
 cursor = connection.cursor()
 
-def insert(lotto_type, data):
+def insert_winning_pairs(lotto_type, data):
     insert = ''
     if lotto_type == 'megamillion':
         insert = "insert into HISTORY_MEGA_MILLION(DRAW_DATE, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, MULTIPLIER) values(" + data + ')'
@@ -17,3 +17,10 @@ def insert(lotto_type, data):
         insert = "insert into HISTORY_POWERBALL(DRAW_DATE, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, MULTIPLIER) values(" + data + ')'
     cursor.execute(insert)
 
+def insert_predicted_numbers(lotto_type, data):
+    insert = ''
+    if lotto_type == 'megamillion':
+        insert = "insert into HISTORY_MEGA_MILLION(PREDECTED_DATE, EMAIL, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6) values(" + data + ')'
+    else:
+        insert = "insert into HISTORY_POWERBALL(PREDECTED_DATE, EMAIL, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, MULTIPLIER) values(" + data + ')'
+    cursor.execute(insert)
