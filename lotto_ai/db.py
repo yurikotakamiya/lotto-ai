29cb6dbc8a476c5f2ce3761e3cf30d04b7ffc11f
yuririnnnu <yrk96424@gmail.com>
2022-05-14 23:18:43 -0400

    function that gets all the phone numbers in place

--- lotto_ai/db.py
+++ lotto_ai/db.py
@@ -5,9 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-cx_Oracle.init_oracle_client(os.environ.get('HOME') + '/Downloads/instantclient_19_8')
-connection = cx_Oracle.connect(user=os.getenv('oracle_user'), password=os.getenv('oracle_password'), dsn=os.getenv('oracle_dsn'))
+cx_Oracle.init_oracle_client(os.environ.get(
+    'HOME') + '/Downloads/instantclient_19_8')
+connection = cx_Oracle.connect(user=os.getenv('oracle_user'), password=os.getenv(
+    'oracle_password'), dsn=os.getenv('oracle_dsn'))
 cursor = connection.cursor()
+
 
 def insert_winning_pairs(lotto_type, data):
     insert = ''
@@ -17,10 +20,18 @@ def insert_winning_pairs(lotto_type, data):
         insert = "insert into HISTORY_POWERBALL(DRAW_DATE, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, MULTIPLIER) values(" + data + ')'
     cursor.execute(insert)
 
+
 def insert_predicted_numbers(lotto_type, data):
     insert = ''
     if lotto_type == 'megamillion':
-        insert = "insert into HISTORY_MEGA_MILLION(PREDECTED_DATE, EMAIL, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6) values(" + data + ')'
+        insert = "insert into PREDICTION_MEGA_MILLION(PREDICTED_DATE, EMAIL, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6) values(" + data + ')'
     else:
-        insert = "insert into HISTORY_POWERBALL(PREDECTED_DATE, EMAIL, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, MULTIPLIER) values(" + data + ')'
+        insert = "insert into PREDICTION_POWERBALL(PREDICTED_DATE, EMAIL, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6) values(" + data + ')'
     cursor.execute(insert)
+
+
+def select_phones():
+    select = "select phone from USERS"
+    phones = [entry[0] for entry in cursor.execute(select)]
+
+    return phones
